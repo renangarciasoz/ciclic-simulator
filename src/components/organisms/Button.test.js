9ebdeda49ff9bfc59fe2af6ece3938ccb831f7d4
Renan Garcia <rsgarcia@uolinc.com>
2018-10-11 11:01:58 -0300

    Add enzyme for unit tests

---
+++ src/components/organisms/Button.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './Button';
+
+const wrap = (props = {}) => shallow(<Button {...props} />).dive();
+
+it('renders with disable props', () => {
+  wrap({ disabled: true });
+});
+
+it('renders children when passed in', () => {
+  const wrapper = wrap({ children: 'test' });
+  expect(wrapper.contains('test')).toBe(true);
+});
+
+it('renders props when passed in', () => {
+  const wrapper = wrap({ type: 'submit' });
+  expect(wrapper.find({ type: 'submit' })).toHaveLength(1);
+});
+
+it('renders button by default', () => {
+  const wrapper = wrap();
+  expect(wrapper.find('button')).toHaveLength(1);
+});
+
+it('renders Link when to is passed in', () => {
+  const wrapper = wrap({ to: 'test' }).dive();
+  expect(wrapper.find('Link')).toHaveLength(1);
+});
